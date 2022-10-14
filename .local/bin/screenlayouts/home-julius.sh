5f3a37c8952409db8773dc2986e19c5fc58a653b
Jxstxs <juleb0202@gmail.com>
2022-10-14 09:31:18 +0200

    feat: added screenlayouts

---
+++ .local/bin/screenlayouts/home-julius.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1080 --pos 320x1440 --rotate normal --set TearFree on --output HDMI-A-0 --rate 60 --mode 2560x1440 --pos 0x0 --rotate normal --set TearFree on --output DisplayPort-0 --off --output DisplayPort-1 --off
+backgroundsetter 2560x2520
